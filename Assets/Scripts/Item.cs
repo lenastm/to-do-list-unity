be83931b4406c928b341ecff8ba9e67ea819ef86
Léna Santamaria <40292041+lenastm@users.noreply.github.com>
2021-01-20 14:47:48 +0100

    debut des scripts

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Item : MonoBehaviour { 
+
+    // public Image isChecked;
+    public TextMeshProUGUI itemText;
+    public int index;
+
+public void SetItem(string itemDescription, int index){
+/*    isChecked.sprite = itemSprite;*/
+        itemText.text = itemDescription;
+        this.index = index;
+}
+
+  
+}
---
+++ Assets/Scripts/ItemsManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+
+public class ItemsManager : MonoBehaviour
+{
+
+    public GameObject itemPrefab;
+    public Transform itemParent;
+    public Button createBtn;
+    public int index = 3;
+
+    private List<Item> items = new List<Item>();
+
+
+
+
+    public void CreateNewItem()
+    {
+
+        CreateItem(" ", 4);
+        
+    }
+
+    public void CreateItem(string itemDescription, int index)
+    {
+        GameObject newItem = (GameObject)PrefabUtility.InstantiatePrefab(itemPrefab, itemParent);
+        Item newItemComponent = newItem.GetComponent<Item>();
+        newItemComponent.SetItem(itemDescription, index);
+        items.Add(newItemComponent);
+        newItemComponent.GetComponent<Toggle>().onValueChanged.AddListener(delegate { checkItem(newItemComponent); });
+    }
+
+
+    void checkItem(Item item)
+    {
+        Debug.Log("item checked");
+    }
+}
